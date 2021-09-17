d0e2f2c39c505a2e755cf04cf098e9d4a23cabf2
blackCracker <fogxcfg@gmail.com>
2021-09-17 09:52:13 +0530

    create photo card

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        <img src="https://source.unsplash.com/random" alt="" className="w-full" />
+
+        <div className="px-6 py-4">
+            <div className="font-bold text-purple-500 text-xl mb-2">
+                Photo By John
+            </div>
+            <ul>
+                <li>
+                  <strong>Views : </strong>
+                  1000
+                </li>
+
+                <li>
+                  <strong>Downloads : </strong>
+                  2000
+                </li>
+
+                <li>
+                  <strong>Likes : </strong>
+                  3000
+                </li>
+            </ul>
+        </div>
+
+        <div className="px-6 py-4">
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+            #tag1
+          </span>
+
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+            #tag2
+          </span>
+
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+            #tag3
+          </span>
+        </div>
     </div>
+
+    // 15.00
   );
 }
 
